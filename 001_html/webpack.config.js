82259ea421bf044e4ffb6ba1f444dac51b202946
amtsik <amtsik@email.ua>
2017-08-09 18:31:32 +0300

    comit

--- 001_html/webpack.config.js
+++ 001_html/webpack.config.js
@@ -1,63 +1,29 @@
-// global.Promise = require('bluebird');
-
-const webpack = require('webpack');
-const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-// const CleanWebpackPlugin = require('clean-webpack-plugin');
-
-const publicPath = 'http://localhost:8050/public/assets';
-const cssName = process.env.NODE_ENV === 'production' ? 'styles-[hash].css' : 'styles.css';
-const jsName = process.env.NODE_ENV === 'production' ? 'bundle-[hash].js' : 'bundle.js';
-
-const plugins = [
-    new webpack.DefinePlugin({
-        'process.env': {
-            BROWSER: JSON.stringify(true),
-            NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
-        },
-    }),
-    new ExtractTextPlugin(cssName),
-    new webpack.LoaderOptionsPlugin({
-        debug: process.env.NODE_ENV !== 'production',
-    }),
-];
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    resolve: {
-        modules: [
-            path.join(__dirname, 'src'),
-            'node_modules',
-        ],
-        extensions: ['.js'],
-    },
-    plugins,
+    context: __dirname,
+    devtool: "source-map",
+    entry: "./js_dev/main.js",
     output: {
-        path: `${__dirname}/vendor`,
-        filename: jsName,
-        publicPath,
+        path: __dirname + "/vendor/js",
+        filename: "build.js"
     },
-    module: {
-        rules: [
+    module:{
+        loaders: [
             {
-                test: /(\.css|\.scss)$/,
+                test: /\.css_dev\.css$/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                    use: [
-                        'css-loader',
-                        'postcss-loader',
-                        'sass-loader',
-                    ],
-                }),
-            },
-            { test: /\.gif$/, loader: 'url-loader?limit=10000&mimetype=image/gif' },
-            { test: /\.jpg$/, loader: 'url-loader?limit=10000&mimetype=image/jpg' },
-            { test: /\.png$/, loader: 'url-loader?limit=10000&mimetype=image/png' },
-            { test: /\.svg/, loader: 'url-loader?limit=26000&mimetype=image/svg+xml' },
-            { test: /\.(woff|woff2|ttf|eot)/, loader: 'url-loader?limit=1' },
-        ],
+                    use: 'css-loader'
+                })
+            }
+        ]
     },
-    devtool: process.env.NODE_ENV !== 'production' ? 'source-map' : null,
+    plugins: [
+        new ExtractTextPlugin('styles.css')
+    ],
     devServer: {
-        headers: { 'Access-Control-Allow-Origin': '*' },
+        inline:true,
+        port: 10000
     },
 };
